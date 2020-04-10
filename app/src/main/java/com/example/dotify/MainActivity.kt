a3131da5a0bd995b00ce0595a304e3b164b00787
Nikolai Frank Nilsen <franknilsen9@gmail.com>
2020-04-09 17:04:58 -0700

    Add functionality for changing username

--- app/src/main/java/com/example/dotify/MainActivity.kt
+++ app/src/main/java/com/example/dotify/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.dotify
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,7 +15,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         assignRandomPlays()
+
+        username_button.setOnClickListener {
+            changeNameClicked()
+        }
+
+        prev_button.setOnClickListener {
+            prevClicked()
+        }
+
+        play_button.setOnClickListener {
+            playClicked()
+        }
+
+        next_button.setOnClickListener {
+            nextClicked()
+        }
     }
 
     private fun assignRandomPlays() {
@@ -22,26 +41,49 @@ class MainActivity : AppCompatActivity() {
         playsText.text = "$randomNum plays"
     }
 
-    fun changeNameClicked() {
+    private fun changeNameClicked() {
+        val changeButton = findViewById<Button>(R.id.username_button)
+        val userText = findViewById<TextView>(R.id.username_text)
+        val editText = findViewById<EditText>(R.id.username_edit)
 
+        userText.visibility = View.GONE
+        editText.visibility = View.VISIBLE
+
+        changeButton.apply {
+            text = "Apply"
+            setOnClickListener {
+                applyClicked(changeButton, userText, editText)
+            }
+        }
     }
 
-    fun applyClicked() {
+    private fun applyClicked(changeButton: Button, userTextView: TextView, userEditText: EditText) {
 
+        userTextView.visibility = View.VISIBLE
+        userEditText.visibility = View.GONE
+
+        val newUserName = userEditText.text.toString()
+        userEditText.setText("")
+
+        userTextView.text = newUserName
+
+        changeButton.setOnClickListener {
+            changeNameClicked()
+        }
     }
 
-    fun prevClicked(view: View) {
+    private fun prevClicked() {
         Toast.makeText(this, "Skipping to previous track", Toast.LENGTH_SHORT).show()
     }
 
-    fun playClicked(view: View) {
+    private fun playClicked() {
         val playsText = findViewById<TextView>(R.id.plays_text)
         val numPlaysString = playsText.text.toString().split(" ")[0]
         val numPlays = numPlaysString.toInt() + 1
         playsText.text = "$numPlays plays"
     }
 
-    fun nextClicked(view: View) {
+    private fun nextClicked() {
         Toast.makeText(this, "Skipping to next track", Toast.LENGTH_SHORT).show()
     }
 
